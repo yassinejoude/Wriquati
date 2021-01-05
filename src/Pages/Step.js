9c6a065015f1d1fce8c72fdf8b4bb7a2bb765baa
Yassine Ejjoud <loadingjoude99@gmail.com>
2021-01-05 17:35:09 +0100

    .

--- src/Pages/Step.js
+++ src/Pages/Step.js
@@ -1,140 +1,129 @@
 import React, { useState, useEffect } from "react";
+import ReactScrollWheelHandler from "react-scroll-wheel-handler";
 import Step1Ulis from "./Step1";
 import Step2Ulis from "./Step2";
 import Step3Ulis from "./Step3";
 import Step4Ulis from "./Step4";
 
-const CircleColors = ["0", "#74daee", "#f98704", "#b85eff", "#14c4a6"];
-const stepTitleColors = ["0", "74daee", "#f98704", "#d190f6", "#14c4a6"];
+const circleColors = ["0", "#74daee", "#f98704", "#b85eff", "#14c4a6"];
+const stepTitleColors = ["0", "#74daee", "#f98704", "#d190f6", "#14c4a6"];
+const stepTitles = [
+  "0",
+  "STEP 1 TITLE",
+  "STEP 2 TITLE",
+  "STEP 3 TITLE",
+  "STEP 4 TITLE",
+];
 
 const Step = () => {
+  const [stepSpec, setStepSpec] = useState({
+    stepOrder: 1,
+    stepCircleColor: circleColors[1],
+    stepTitle: stepTitles[1],
+    stepTitleColor: stepTitleColors[1],
+  });
 
-  const [StepOrder, setStepOrder] = useState(1);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-  },[5]);
-
-  const handleScroll = (e) => {
-    setStepOrder(3)
+  //   const handleScroll = (e) => {
+  //     setStepOrder(3);
+  //   };
+  const updateStep = (order) => {
+    setStepSpec({
+      stepOrder: order,
+      stepCircleColor: circleColors[order],
+      stepTitle: stepTitles[order],
+      stepTitleColor: stepTitleColors[order],
+    });
+  };
+  const prevOrder = () => {
+    if (stepSpec.stepOrder > 1 && stepSpec.stepOrder <= 4) {
+      updateStep(stepSpec.stepOrder - 1);
+    }
+  };
+  const nextOrder = () => {
+    if (stepSpec.stepOrder >= 1 && stepSpec.stepOrder < 4) {
+      updateStep(stepSpec.stepOrder + 1);
+    }
   };
 
   return (
-    <svg
+    <ReactScrollWheelHandler
+      upHandler={prevOrder}
+      downHandler={nextOrder}
+      wheelconfig={[1, 500, 0.05]}
       style={{
-        display: "block",
-        margin: "auto",
-        width: "80%",
+        width: "100%",
+        height: "80vh",
+        outline: "none",
       }}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 1227.2 534.13"
     >
-      <defs>
-        <clipPath id="clip-path">
-          <path fill="none" d="M0 0H583.46V534.13H0z"></path>
-        </clipPath>
-      </defs>
-      <g id="Layer_2" data-name="Layer 2">
-        <g id="Layer_1-2" data-name="Layer 1">
-          <g id="step_2:_find_and_upload" data-name="step 2: find and upload">
-            {StepOrder === 1 && <Step1Ulis />}
-            {StepOrder === 2 && <Step2Ulis />}
-            {StepOrder === 3 && <Step3Ulis />}
-            {StepOrder === 4 && <Step4Ulis />}
+      <svg
+        style={{
+          display: "block",
+          margin: "auto",
+          width: "80%",
+        }}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1256.82 621.1"
+      >
+        <defs>
+          <clipPath id="clip-path">
+            <path fill="none" d="M0 0H583.46V534.13H0z"></path>
+          </clipPath>
+        </defs>
+        <g id="Layer_2" data-name="Layer 2">
+          <g id="Layer_1-2" data-name="Layer 1">
+            <g id="step_2:_find_and_upload" data-name="step 2: find and upload">
+              {stepSpec.stepOrder === 1 && <Step1Ulis />}
+              {stepSpec.stepOrder === 2 && <Step2Ulis />}
+              {stepSpec.stepOrder === 3 && <Step3Ulis />}
+              {stepSpec.stepOrder === 4 && <Step4Ulis />}
 
-            <g id="stepCircle">
-              <path
-                id="circle"
-                fill="#f98704"
-                d="M584.6 168.94A68.07 68.07 0 11516.17 237a68.26 68.26 0 0168.43-68.06z"
-              ></path>
+              <g id="stepCircle">
+                <path
+                  id="circle"
+                  fill={stepSpec.stepCircleColor}
+                  d="M584.6 168.94A68.07 68.07 0 11516.17 237a68.26 68.26 0 0168.43-68.06z"
+                ></path>
+                <text
+                  id="_2"
+                  fill="#fff"
+                  data-name="2"
+                  fontFamily="SegoeUI-Bold, Segoe UI"
+                  fontSize="62.54"
+                  fontWeight="700"
+                  transform="translate(566.61 262.4)"
+                  style={{ isolation: "isolate" }}
+                >
+                  {stepSpec.stepOrder}
+                </text>
+                <text
+                  id="stepText"
+                  fill="#707070"
+                  fontFamily="MyriadPro-Regular, Myriad Pro"
+                  fontSize="32"
+                  transform="translate(620.37 350.4)"
+                >
+                  HI just a question on es lint. what is the \n standard way of
+                  setting it up? just install the package from atom or setting
+                  up es lint locally for each and every project.
+                </text>
+              </g>
               <text
-                id="_2"
-                fill="#fff"
-                data-name="2"
+                id="stepTitle"
+                fill={stepSpec.stepTitleColor}
                 fontFamily="SegoeUI-Bold, Segoe UI"
-                fontSize="62.54"
+                fontSize="36.79"
                 fontWeight="700"
-                transform="translate(566.61 262.4)"
+                transform="translate(685.06 256.09)"
                 style={{ isolation: "isolate" }}
               >
-                2
+                {stepSpec.stepTitle}
               </text>
             </g>
-            <g
-              id="stepText"
-              style={{ isolation: "isolate" }}
-              fill="#707070"
-              fontFamily="SegoeUI, Segoe UI"
-              fontSize="27.23"
-            >
-              <text
-                transform="translate(606.94 367.84)"
-                style={{ isolation: "isolate" }}
-              >
-                Lorem ipsum dolor sit amet, consectetur adipiscing
-              </text>
-              <text
-                transform="translate(606.94 403.9)"
-                style={{ isolation: "isolate" }}
-              >
-                elit,sed do eiusmod tempor incididunt ut labore et
-              </text>
-              <text
-                transform="translate(606.94 439.95)"
-                style={{ isolation: "isolate" }}
-              >
-                dolore magna aliqua. Ut enim ad minim veniam,
-              </text>
-              <text
-                transform="translate(606.94 476.01)"
-                style={{ isolation: "isolate" }}
-              >
-                exercitation ullamco laboris nisi ut aliquip ex ea
-              </text>
-              <text
-                transform="translate(606.94 512.07)"
-                style={{ isolation: "isolate" }}
-              >
-                consequat. Duis aute irure dolor in
-              </text>
-            </g>
-            <text
-              id="stepTitle"
-              fill="#f98704"
-              fontFamily="SegoeUI-Bold, Segoe UI"
-              fontSize="36.79"
-              fontWeight="700"
-              transform="translate(685.06 256.09)"
-              style={{ isolation: "isolate" }}
-            >
-              USE WRI
-              <tspan x="149.61" y="0" letterSpacing="-.01em">
-                Q
-              </tspan>
-              <tspan x="177.15" y="0" letterSpacing="-.07em">
-                A
-              </tspan>
-              <tspan x="200.45" y="0">
-                TI&apos;S S
-              </tspan>
-              <tspan x="295.84" y="0" letterSpacing=".01em">
-                E
-              </tspan>
-              <tspan x="315.94" y="0">
-                A
-              </tspan>
-              <tspan x="341.81" y="0" letterSpacing="-.01em">
-                R
-              </tspan>
-              <tspan x="365.47" y="0">
-                CH
-              </tspan>
-            </text>
           </g>
         </g>
-      </g>
-    </svg>
+      </svg>
+    </ReactScrollWheelHandler>
   );
 };
 
